fca380c73afc9268cd9ad3697ab960087757e972
ali <aliibrahim4793@gmail.com>
2019-07-13 11:57:29 +0300

    task 7 commit

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -5,3 +5,10 @@ class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = '__all__'
+class UpdateRestaurantForm(forms.ModelForm):
+	class Meta:
+		model=Restaurant
+		fields=["description","opening_time","closing_time"]
+
+
+
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm, UpdateRestaurantForm
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
@@ -31,10 +31,21 @@ def restaurant_create(request):
     return render(request, 'create.html', context)
 
 def restaurant_update(request, restaurant_id):
+    restaurant =  Restaurant.objects.get(id=restaurant_id)
+    form = UpdateRestaurantForm(instance=restaurant)
+    if request.method == "POST":
+        form = UpdateRestaurantForm(request.POST, instance=restaurant)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurant-list')
 
-    return
+    context = {
+        "form" : form,
+        "id" : restaurant_id
+    }
+    return render(request, 'update.html', context)
 
 def restaurant_delete(request, restaurant_id):
-
-
-    return
+    Restaurant.objects.get(id=restaurant_id).delete()
+    
+    return redirect("restaurant-list")
